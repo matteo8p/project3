96beef768e5ff2418fcf43663c40470281cf2b70
Matthew Wang <matt8p@outlook.com>
2020-10-22 20:02:58 -0700

    m

--- tcb.h
+++ tcb.h
@@ -14,6 +14,6 @@ void init_TCB(TCB_t *tcb, void *function, void *stackP, int stack_size, int id) 
 	getcontext(&tcb->context);              
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
-	makecontext(&tcb->context, function, 0, id);
+	makecontext(&tcb->context, function, 2, id);
 }
 
