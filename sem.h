efc2367db30b15bc22cdd360f4840153ee8aec9f
Matthew Wang <matt8p@outlook.com>
2020-10-23 16:51:05 -0700

    m

--- sem.h
+++ sem.h
@@ -36,12 +36,11 @@ void P(semaphore *sem, int id)
 
 void V(semaphore *sem) 
 {
-	sem->value++;
 	if(sem->value <= 0 && sem->semQ != NULL)
 	{
 		struct TCB_t *tcb = delQueue(sem->semQ);
 		addQueue(runQ, tcb);
 	}
-
+	sem->value++;
 	yield(); 
 }
