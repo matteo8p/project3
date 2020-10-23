7797976c5e789a6f7b243627839e74ee6f13c8dd
Matthew Wang <matt8p@outlook.com>
2020-10-22 17:09:34 -0700

    m

--- sem.h
+++ sem.h
@@ -13,11 +13,14 @@ void InitSem(struct sem *semaphore, int thisValue)      //Initialize value field
 
 void P(struct sem *semaphore)
 {
-	semaphore->val--;
-	if (semaphore->val <= 0) {
+	if (semaphore->val == 0) {
 		struct TCB_t *t = DelQueue(runQ); 
 		AddQueue(&(semaphore->q), t);
 		swapcontext(&(t->context), &(runQ->context)); 
+	}else
+	{
+		semaphore->val--;
+		return; 
 	}
 }
 
