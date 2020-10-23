466947adeb724d25adb1818b5598a4a8937c2daf
Matthew Wang <matt8p@outlook.com>
2020-10-23 09:17:24 -0700

    m

--- sem.h
+++ sem.h
@@ -23,41 +23,36 @@ void initSem(semaphore *sem, int value) {
 }
 
 void P(semaphore *sem, int id, bool producer) {
-	bool shownOnce = false; 
-	while(1)
+
+	if (sem->value == 0) 
 	{
-		if (sem->value == 0) {
-			if(shownOnce == false)
-			{
-				if(producer)
-				{
-					printf("\n Producer %d is waiting \n", id); 
-				}else
-				{
-					printf("\n Consumer %d is waiting \n", id); 
-				}
-				shownOnce = true; 
-			}
-			struct TCB_t *t = delQueue(runQ);
-			addQueue(sem->sleepQ, t);
-			// swapcontext(&(t->context), &(runQ->header->context));
-			yield(); 
+		if(producer)
+		{
+			printf("\n Producer %d is waiting \n", id); 
 		}else
 		{
-			sem->value--;
-			return; 
+			printf("\n Consumer %d is waiting \n", id); 
 		}
+		
+		struct TCB_t *t = delQueue(runQ);
+		addQueue(sem->sleepQ, t);
+		swapcontext(&(t->context), &(runQ->header->context));
+	}else
+	{
+		sem->value--;
+		return; 
 	}
+	
 }
 
 void V(semaphore *sem) {
 	struct TCB_t *t; 
 	sem->value++;
-	// if(sem->value <= 0)
-	// {
+	if(sem->value <= 0)
+	{
 		t = delQueue(sem->sleepQ);
 		addQueue(runQ, t);
-	// }
+	}
 	//yield(); 
 }
 
