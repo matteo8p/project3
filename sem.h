66b63815e368f06bf9317f956cb0fc16041557f1
Matthew Wang <matt8p@outlook.com>
2020-10-22 23:58:54 -0700

    m

--- sem.h
+++ sem.h
@@ -1,52 +1,29 @@
-// Name: Cory Siebler & Marcus Finney
-// Class: CSE430 #12109
-// Assignment: Project 3
-// Description: Holds the functions to create and modify Semaphores. InitSem(semaphore, value) initializes
-//		a new Semaphore with the value equal to the parameter. P(semaphore) will decrement the value
-//		and block the process if the value is less than zero. V(semaphore) will increment the value
-//		and call the aprocess in the PCB and puts in the RunQ if the value is 0 or negative.
-
 #ifndef SEM_H
 #define SEM_H
 
-//---------------//
-// Include Files //
-//---------------//
 #include "threads.h"
 
-//----------------------------//
-// Data Structure Declaration //
-//----------------------------//
 typedef struct semaphore {
-	int value;	// Holds the Semaphore value that is incremented & decremented
-	struct queue *sleepQ;	// Stores the Queue to hold the blocked processes
+	int value;	
+	struct queue *sleepQ;	
 } semaphore;
 
-//----------------------------//
-// Method Forward Declaration //
-//----------------------------//
 void initSem(semaphore*, int);
 void P(semaphore*);
 void V(semaphore*);
 
-//----------------//
-// initSem Method //
-//----------------//
 void initSem(semaphore *sem, int value) {
-	// Allocate memory for the Sleep Queue
 	sem->sleepQ = (struct queue*) malloc(sizeof(struct queue));
-	
-	// Initialize the Head of the Sleep Queue
+
 	initQueue(sem->sleepQ);
 
-	// Initialize the value of the Semaphore
 	sem->value = value;
 
 	return;
 }
 
 void P(semaphore *sem) {
-	// Declare a temporary TCB to hold the popped process
+
 	struct TCB_t *p; 
 
 	if (sem->value == 0) {
