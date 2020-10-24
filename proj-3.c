014935e77276c973d3add9cffb36b66a29a9872c
Matthew Wang <matt8p@outlook.com>
2020-10-23 18:02:08 -0700

    m

--- proj-3.c
+++ proj-3.c
@@ -63,9 +63,7 @@ void producer(int id)
         V(full);
         i++; 
     }
-    TCB_t *tcb = delQueue(runQ); 
     if(tcb == NULL) exit(0); 
-    swapcontext(&(tcb->context), &(runQ->headPointer->context)); 
 }
 
 void consumer(int id)
@@ -82,9 +80,7 @@ void consumer(int id)
         V(empty); 
         i++; 
     }
-    TCB_t *tcb = delQueue(runQ); 
-    if(tcb == NULL) exit(0); 
-    swapcontext(&(tcb->context), &(runQ->headPointer->context)); 
+    if(runQ->headPointer == NULL) exit(0); 
 }
 
 
