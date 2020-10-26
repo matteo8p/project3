78ccb1c494a2b9f8686244cdf470c7dc68acc144
Matthew Wang <matt8p@outlook.com>
2020-10-26 16:40:55 -0700

    m

--- q.h
+++ q.h
@@ -34,8 +34,8 @@ void addQueue(struct queue *head, struct TCB_t *item) {
 		}
 	} else {
 		head->headPointer = item; 
-		item->prev = item; 
-		item->next = item; 
+		head->headPointer->prev = head; 
+		head->headPointer->next = head; 
 	}
 	return;
 }
