c28015385f73b57b17bdec2ae7e9226b1939344f
Matthew Wang <matt8p@outlook.com>
2020-10-27 16:03:49 -0700

    m

--- threads.h
+++ threads.h
@@ -20,12 +20,16 @@ void run() {
 }
 
 void yield() {
-	ucontext_t context_a; 
-	ucontext_t context_b; 
-	getcontext(&context_a);
-	context_a = runQ->headPointer->context = context_a;
-	rotQueue(runQ);
-	context_b = runQ->headPointer->context;
-	swapcontext(&context_a, &context_b);
+	// ucontext_t context_a; 
+	// ucontext_t context_b; 
+	// getcontext(&context_a);
+	// context_a = runQ->headPointer->context = context_a;
+	// rotQueue(runQ);
+	// context_b = runQ->headPointer->context;
+	// swapcontext(&context_a, &context_b);
+
+	TCB_t *temp = runQ->headPointer; 
+	rotateQ(runQ);
+	swapcontext(&(temp->context), &(runQ->headPointer->context));
 }
 
